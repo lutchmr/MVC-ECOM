2b799b2d2ae438725e17c1d06743ecc1fa5683db
lutchmr <lutchmr@gmail.com>
2022-11-29 13:39:43 +0200

    first commit

---
+++ Ecom_Project/Controllers/HomeController.cs
@@ -0,0 +1,293 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Ecom_Project.Models;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+namespace Ecom_Project.Controllers;
+
+public class HomeController : Controller
+{
+    private string constr = "server=localhost;port=3306;uid=root;pwd=Lambo@786;database=furndb;charset=utf8;sslmode=none;AllowPublicKeyRetrieval=True";
+    //private string mail;
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult privacy()
+
+    {
+        return View();
+    
+    }
+    public IActionResult register()
+        {
+            return View();
+        }
+        public IActionResult login()
+        {
+            return View();
+        }
+[HttpPost]
+        public ActionResult Register(UserModel obj)
+        {
+            ViewBag.first_name = obj.first_name;
+            ViewBag.last_name = obj.last_name;
+            ViewBag.email_id = obj.email_id;
+            ViewBag.password = obj.password;
+
+           // mail=obj.email_id;
+            //string enPassword = obj.EncodePasswordToBase64(obj.Password);
+            //string dePassword = obj.DecodeFrom64(enPassword);
+            //ViewBag.enPassword = enPassword;
+            //ViewBag.dePassword = dePassword;
+            MySqlConnection conn = new MySqlConnection(constr);
+            using (conn)
+            {
+                try
+                {
+                    List <UserModel> user = new List<UserModel>();
+                    conn.Open();
+                    MySqlCommand command =  conn.CreateCommand();
+                    command.CommandType = System.Data.CommandType.Text;
+                    string? fname = obj.first_name;
+                    string? lname = obj.last_name;
+                    string? email = obj.email_id;
+                    string? pword = obj.password;
+                   // reader.Close();
+                   Console.WriteLine("fname: " + fname);
+                    command.CommandText=$"Insert into customer(first_name, last_name, email_id, password) values('{fname}','{lname}','{email}','{pword}');";
+                    MySqlDataReader reader = command.ExecuteReader();
+                    reader.Close();
+                    conn.Close();
+                    System.Console.WriteLine("Connection is : " + conn.State.ToString() + Environment.NewLine);
+                    // return StatusCode(200);
+                }catch(MySql.Data.MySqlClient.MySqlException ex){
+                    System.Console.WriteLine("Error: " + ex.Message.ToString());
+                    conn.Close();
+                    System.Console.WriteLine("Connection is : " + conn.State.ToString() + Environment.NewLine);
+                    return StatusCode(500);
+                }finally {
+                    //System.Console.WriteLine("Press any key to Exit...");
+                    //
+                    //Console.ReadKey();
+                }
+            // if (chkAddon != null)
+            //     ViewBag.Addon = "Selected";
+            // else
+            //     ViewBag.Addon = "Not Selected";
+            return View(obj);
+            // return View("Login", obj);
+            }
+           
+        }
+        
+     
+
+
+        // public IActionResult login()
+        // {
+        //     List <UserModel> user = new List<UserModel>();
+        // // ViewBag.first_name = obj.first_name;
+        // // ViewBag.last_name = obj.last_name;
+        // //ViewBag.email_id = obj.email_id;
+        // //ViewBag.password = obj.password;
+        // //string enPassword = obj.EncodePasswordToBase64(obj.Password);
+        //  //string dePassword = obj.DecodeFrom64(enPassword);
+        //  //ViewBag.enPassword = enPassword;
+        //  //ViewBag.dePassword = dePassword;
+        //     MySqlConnection conn = new MySqlConnection(constr);
+        //     using (conn)
+        
+        //         {
+                    
+        //             string query=$"select * from customer where email_id='$email_id' and  password='$password'  ";
+                  
+        //             if (query is not null  )
+        //             {
+                    
+        //             System.Console.WriteLine("Successfully logged in!");
+        //             System.Console.WriteLine("Connection is : " + conn.State.ToString() + Environment.NewLine);
+        //             }
+                    
+        //             {
+        //             System.Console.WriteLine( "Email or Password is incorrect!");
+        //             System.Console.WriteLine("Connection is : " + conn.State.ToString() + Environment.NewLine);
+        //             // return View(Index());
+        //             }
+        //             using (MySqlCommand cmd = new MySqlCommand(query))
+        //             {
+        //                 cmd.Connection = conn;
+        //                 conn.Open();
+        //                 // using  (MySqlDataReader sdr = cmd.ExecuteReader())
+                        
+        //                 // {
+        //                 // while (sdr.Read())
+        //                 // {
+        //                 //     user.Add(new UserModel
+        //                 //     {
+        //                 //      //first_name=string.Format("{0:S}", sdr["first_name"]),
+        //                 //      //last_name=string.Format("{0:S}", sdr["last_name"]),
+        //                 //      email_id=string.Format("{0:S}", sdr["email_id"]),
+        //                 //      password=string.Format("{0:S}", sdr["password"]),
+        //                 //     });
+        //                 // } 
+                       
+        //                 // }
+        //                 conn.Close();
+        //             }
+
+        //         }
+
+        //         return View(user);
+        // }
+                            
+      public IActionResult about()
+        {
+            return View();
+        }                       
+        public IActionResult contact()
+        {
+            return View();
+        }                       
+        public IActionResult terms()
+        {
+            return View();
+        }    
+        public IActionResult faq()
+        {
+            return View();
+        }  
+          public IActionResult product()
+        {
+            return View();
+        }     
+         public IActionResult cart()
+        {
+            return View();
+        }   
+        public IActionResult beds()
+        {
+            return View();
+        }   
+         public IActionResult homeoffice()
+        {
+            return View();
+        }   
+
+        public IActionResult dining()
+        {
+            return View();
+        }   
+         public IActionResult recliners()
+        {
+            return View();
+        }   
+        public IActionResult checkout()
+        {
+            return View();
+        }   
+
+         public IActionResult order()
+        {
+            return View();
+        }   
+        public IActionResult view()
+        {
+            return View();
+        }   
+
+         public IActionResult help()
+        {
+            return View();
+        }   
+        
+         public IActionResult service()
+        {
+            return View();
+        }  
+        public IActionResult shop()
+        {
+            return View();
+        }  
+          
+           public IActionResult payment()
+        {
+            return View();
+        }  
+        public IActionResult money()
+        {
+            return View();
+        }  
+        public IActionResult returns()
+        {
+            return View();
+        }  
+        public IActionResult shipping()
+        {
+            return View();
+        }  
+
+        public IActionResult wish()
+        {
+            return View();
+        }  
+
+        public IActionResult track()
+        {
+            return View();
+        }   
+
+        // public IActionResult login1()
+        // {
+        //     return View();
+        // }                     
+                                                                                  
+   [HttpPost]
+    public IActionResult login(loginModel obj)
+   {
+        MySqlConnection conn = new MySqlConnection(constr);
+        using (conn)
+        {
+             var uname = obj.email_id;
+             var pword = obj.password;
+            string query = $"SELECT email_id, password FROM customer where email_id = '{uname}' and password ='{pword}';";
+    
+            using (MySqlCommand cmd = new MySqlCommand(query))
+            {
+                cmd.Connection = conn;
+                conn.Open();
+                using (MySqlDataReader sdr = cmd.ExecuteReader())
+                {
+                    while (sdr.Read())
+                    {
+                        obj.password = string.Format("{0:S}", sdr["password"]);
+                        if (string.Compare(obj.password,string.Format("{0:S}", sdr["password"]),false)==0)
+                            return View("Index");
+                        else
+                            return View("privacy");                        
+                    }
+                }
+                conn.Close();
+            }
+        }        
+        return View(obj);
+    }
+
+  
+  
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Ecom_Project/wwwroot/js/site.js
@@ -0,0 +1,130 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+var sortBtn = document.querySelector('.filter-menu').children;
+var sortItem = document.querySelectorAll('.row>div');
+console.log(sortBtn, sortItem);
+for (var i = 0; i < sortBtn.length; i++) {
+    sortBtn[i].addEventListener('click', function () {
+        // removing class current from all li items of ul class filter-menu
+        for (var j = 0; j < sortBtn.length; j++) {
+            //removing currect class from all buttons
+            sortBtn[j].classList.remove('current');
+        }
+        console.log(this);
+        //this is regering to sortBtn[i]
+        this.classList.add('current');
+        var targetData = this.getAttribute('data-target');//getting the data-target value
+        // console.log(targetData);// data-target value will be displayed e.g. js
+        for (var k = 0; k < sortItem.length; k++) {
+            sortItem[k].classList.remove('active');
+            sortItem[k].classList.add('delete');
+            // console.log(sortItem[k].getAttribute('data-item')); 
+            if (sortItem[k].getAttribute('data-item') == targetData || targetData == "Reset") {
+                sortItem[k].classList.remove('delete');
+                sortItem[k].classList.add('active');
+            }
+        }
+    });
+}
+
+
+
+
+
+
+
+
+/*login */
+// function login()
+// 	{
+// 		if (query != null) 
+// 		{
+
+// 			alert('Thank You for Login & You are Redirecting to Classy Furn Website');
+// 			//Redirecting to other page or webste code or you can set your own html page.
+// 				 window.location = "https://localhost:7288/";
+// 					  }
+
+		
+// 		else{
+
+// 			alert("please enter authenticated user id and password combination or kindly Register and login");
+
+
+
+// 		}
+// 		 var uname = document.getElementById("email_id").value;
+// 		 var pwd = document.getElementById("password").value;
+// 		 var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+// 		 if(uname =='')
+// 		 {
+// 		 	alert("please enter user name.");
+// 		 }
+// 		 else if(pwd=='')
+// 		 {
+//          	alert("enter the password");
+// 		 }
+// 		 else if(!filter.test(uname))
+// 		 {
+// 		 	alert("Enter valid email id.");
+// 		 }
+// 		 else if(pwd.length < 6 || pwd.length > 6)
+// 		 {
+// 		 	alert("Password min and max length is 6.");
+// 		 }
+	
+// 		{
+// // 	alert('Thank You for Login & You are Redirecting to Classy Furn Website');
+// //   //Redirecting to other page or webste code or you can set your own html page.
+// //        window.location = "https://localhost:7288/";
+// 			}
+	
+// 	//Reset Inputfield code.
+// 	function clearFunc()
+// 	{
+// 		document.getElementById("email").value="";
+// 		document.getElementById("pwd1").value="";
+// 	}	
+
+// //Reset Inputfield code.
+//  function clearFunc()
+// {
+// 	 	document.getElementById("email_id").value="";
+// 	document.getElementById("password").value="";
+//  } 	
+// login2	
+
+// $(document).ready(function(){
+// 	$("#login").click(function(){
+// 	var email_id = $("#email_id").val();
+// 	var password = $("#password").val();
+// 	// Checking for blank fields.
+// 	if( email_id =='' || password ==''){
+// 	$('input[type="text"],input[type="password"]').css("border","2px solid red");
+// 	$('input[type="text"],input[type="password"]').css("box-shadow","0 0 3px red");
+// 	alert("Please fill all fields...!!!!!!");
+// 	}else {
+// 	$.post("login.php",{ email_id: email_id, password:password},
+// 	function(data) {
+// 	if(data=='Invalid Email.......') {
+// 	$('input[type="text"]').css({"border":"2px solid red","box-shadow":"0 0 3px red"});
+// 	$('input[type="password"]').css({"border":"2px solid #00F5FF","box-shadow":"0 0 5px #00F5FF"});
+// 	alert(data);
+// 	}else if(data=='Email or Password is wrong...!!!!'){
+// 	$('input[type="text"],input[type="password"]').css({"border":"2px solid red","box-shadow":"0 0 3px red"});
+// 	alert(data);
+// 	} else if(data=='Successfully Logged in...'){
+// 	$("form")[0].reset();
+// 	$('input[type="text"],input[type="password"]').css({"border":"2px solid #00F5FF","box-shadow":"0 0 5px #00F5FF"});
+// 	alert(data);
+// 	} else{
+// 	alert(data);
+// 	}
+// 	});
+// 	}
+// 	});
+// 	});
+
+
